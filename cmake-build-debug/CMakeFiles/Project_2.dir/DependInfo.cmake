27e350729b71461e5e36f50855d5359349576a5d
45222 <peterjuul2@gmail.com>
2024-04-29 23:05:04 +0200

    jeg skal bruge hjælp

---
+++ cmake-build-debug/CMakeFiles/Project_2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/45222/CLionProjects/Project_2_Group_27/controller/controller.c" "CMakeFiles/Project_2.dir/controller/controller.c.obj" "gcc" "CMakeFiles/Project_2.dir/controller/controller.c.obj.d"
+  "C:/Users/45222/CLionProjects/Project_2_Group_27/main.c" "CMakeFiles/Project_2.dir/main.c.obj" "gcc" "CMakeFiles/Project_2.dir/main.c.obj.d"
+  "C:/Users/45222/CLionProjects/Project_2_Group_27/model/model.c" "CMakeFiles/Project_2.dir/model/model.c.obj" "gcc" "CMakeFiles/Project_2.dir/model/model.c.obj.d"
+  "C:/Users/45222/CLionProjects/Project_2_Group_27/view/view.c" "CMakeFiles/Project_2.dir/view/view.c.obj" "gcc" "CMakeFiles/Project_2.dir/view/view.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
